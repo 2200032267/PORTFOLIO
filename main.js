b5b8b64fb31da9bd61ed7fc5239e1714f7f2678c
2200032267 <2200032267@kluniversity.in>
2024-11-12 13:19:12 +0530

    session1

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Full Stack Developer", "Web Designer", "Data Scientist/Analyst","Software Developer"],
+    strings: ["Full Stack Developer", "Web Designer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
